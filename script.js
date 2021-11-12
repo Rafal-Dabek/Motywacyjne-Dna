fee6b9b40f75fe35b86e68be4dd71cee3992046f
Rafal-Dabek <62549937+Rafal-Dabek@users.noreply.github.com>
2021-11-12 22:11:06 +0100

    Some upgrades

--- script.js
+++ script.js
@@ -1,182 +1,4 @@
-function done2()
-{
 
-    value_select_1_1 = parseInt(document.getElementById("select_1_1").value)
-    value_select_1_2 = parseInt(document.getElementById("select_1_2").value)
-    value_select_1_3 = parseInt(document.getElementById("select_1_3").value)
-    value_select_1_4 = parseInt(document.getElementById("select_1_4").value)
-    value_select_1_5 = parseInt(document.getElementById("select_1_5").value)
-    value_select_1_6 = parseInt(document.getElementById("select_1_6").value)
-    value_select_1_7 = parseInt(document.getElementById("select_1_7").value)
-    value_select_1_8 = parseInt(document.getElementById("select_1_8").value)
-
-    value_select_2_1 = parseInt(document.getElementById("select_2_1").value)
-    value_select_2_2 = parseInt(document.getElementById("select_2_2").value)
-    value_select_2_3 = parseInt(document.getElementById("select_2_3").value)
-    value_select_2_4 = parseInt(document.getElementById("select_2_4").value)
-    value_select_2_5 = parseInt(document.getElementById("select_2_5").value)
-    value_select_2_6 = parseInt(document.getElementById("select_2_6").value)
-    value_select_2_7 = parseInt(document.getElementById("select_2_7").value)
-    value_select_2_8 = parseInt(document.getElementById("select_2_8").value)
-
-    value_select_3_1 = parseInt(document.getElementById("select_3_1").value)
-    value_select_3_2 = parseInt(document.getElementById("select_3_2").value)
-    value_select_3_3 = parseInt(document.getElementById("select_3_3").value)
-    value_select_3_4 = parseInt(document.getElementById("select_3_4").value)
-    value_select_3_5 = parseInt(document.getElementById("select_3_5").value)
-    value_select_3_6 = parseInt(document.getElementById("select_3_6").value)
-    value_select_3_7 = parseInt(document.getElementById("select_3_7").value)
-    value_select_3_8 = parseInt(document.getElementById("select_3_8").value)
-
-    value_select_4_1 = parseInt(document.getElementById("select_4_1").value)
-    value_select_4_2 = parseInt(document.getElementById("select_4_2").value)
-    value_select_4_3 = parseInt(document.getElementById("select_4_3").value)
-    value_select_4_4 = parseInt(document.getElementById("select_4_4").value)
-    value_select_4_5 = parseInt(document.getElementById("select_4_5").value)
-    value_select_4_6 = parseInt(document.getElementById("select_4_6").value)
-    value_select_4_7 = parseInt(document.getElementById("select_4_7").value)
-    value_select_4_8 = parseInt(document.getElementById("select_4_8").value)
-
-    value_select_5_1 = parseInt(document.getElementById("select_5_1").value)
-    value_select_5_2 = parseInt(document.getElementById("select_5_2").value)
-    value_select_5_3 = parseInt(document.getElementById("select_5_3").value)
-    value_select_5_4 = parseInt(document.getElementById("select_5_4").value)
-    value_select_5_5 = parseInt(document.getElementById("select_5_5").value)
-    value_select_5_6 = parseInt(document.getElementById("select_5_6").value)
-    value_select_5_7 = parseInt(document.getElementById("select_5_7").value)
-    value_select_5_8 = parseInt(document.getElementById("select_5_8").value)
-
-    value_select_6_1 = parseInt(document.getElementById("select_6_1").value)
-    value_select_6_2 = parseInt(document.getElementById("select_6_2").value)
-    value_select_6_3 = parseInt(document.getElementById("select_6_3").value)
-    value_select_6_4 = parseInt(document.getElementById("select_6_4").value)
-    value_select_6_5 = parseInt(document.getElementById("select_6_5").value)
-    value_select_6_6 = parseInt(document.getElementById("select_6_6").value)
-    value_select_6_7 = parseInt(document.getElementById("select_6_7").value)
-    value_select_6_8 = parseInt(document.getElementById("select_6_8").value)
-
-    value_select_7_1 = parseInt(document.getElementById("select_7_1").value)
-    value_select_7_2 = parseInt(document.getElementById("select_7_2").value)
-    value_select_7_3 = parseInt(document.getElementById("select_7_3").value)
-    value_select_7_4 = parseInt(document.getElementById("select_7_4").value)
-    value_select_7_5 = parseInt(document.getElementById("select_7_5").value)
-    value_select_7_6 = parseInt(document.getElementById("select_7_6").value)
-    value_select_7_7 = parseInt(document.getElementById("select_7_7").value)
-    value_select_7_8 = parseInt(document.getElementById("select_7_8").value)
-
-    if(value_select_1_1+value_select_1_2+value_select_1_3+value_select_1_4+
-      value_select_1_5+value_select_1_6+value_select_1_7+value_select_1_8 != 10)
-    {
-        alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.1")
-    }
-    else if(value_select_2_1+value_select_2_2+value_select_2_3+value_select_2_4+
-      value_select_2_5+value_select_2_6+value_select_2_7+value_select_2_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.2")
-    }
-    else if(value_select_3_1+value_select_3_2+value_select_3_3+value_select_3_4+
-      value_select_3_5+value_select_3_6+value_select_3_7+value_select_3_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.3")
-    }
-    else if(value_select_4_1+value_select_4_2+value_select_4_3+value_select_4_4+
-      value_select_4_5+value_select_4_6+value_select_4_7+value_select_4_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.4")
-    }
-    else if(value_select_5_1+value_select_5_2+value_select_5_3+value_select_5_4+
-      value_select_5_5+value_select_5_6+value_select_5_7+value_select_5_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.5")
-    }
-    else if(value_select_6_1+value_select_6_2+value_select_6_3+value_select_6_4+
-      value_select_6_5+value_select_6_6+value_select_6_7+value_select_6_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.6")
-    }
-    else if(value_select_7_1+value_select_7_2+value_select_7_3+value_select_7_4+
-      value_select_7_5+value_select_7_6+value_select_7_7+value_select_7_8 != 10)
-    {
-      alert("Punkty muszą byc rozdzielone tak aby ich suma wyniosła 10 w pytaniu nr.7")
-    }
-    else
-    {
-
-
-      var org_wynik = parseInt(document.getElementById("select_1_1").value) + parseInt(document.getElementById("select_2_1").value) 
-      + parseInt(document.getElementById("select_3_1").value) + parseInt(document.getElementById("select_4_1").value) 
-      + parseInt(document.getElementById("select_5_1").value) + parseInt(document.getElementById("select_6_1").value) + parseInt(document.getElementById("select_7_1").value) 
-    
-    
-      var nl_wynik = parseInt(document.getElementById("select_1_2").value) + parseInt(document.getElementById("select_2_2").value) 
-      + parseInt(document.getElementById("select_3_2").value) + parseInt(document.getElementById("select_4_2").value) 
-      + parseInt(document.getElementById("select_5_2").value) + parseInt(document.getElementById("select_6_2").value) + parseInt(document.getElementById("select_7_2").value) 
-    
-      var des_wynik = parseInt(document.getElementById("select_1_3").value) + parseInt(document.getElementById("select_2_3").value) 
-      + parseInt(document.getElementById("select_3_3").value) + parseInt(document.getElementById("select_4_3").value) 
-      + parseInt(document.getElementById("select_5_3").value) + parseInt(document.getElementById("select_6_3").value) + parseInt(document.getElementById("select_7_3").value) 
-    
-      var kre_wynik = parseInt(document.getElementById("select_1_4").value) + parseInt(document.getElementById("select_2_4").value) 
-      + parseInt(document.getElementById("select_3_4").value) + parseInt(document.getElementById("select_4_4").value) 
-      + parseInt(document.getElementById("select_5_4").value) + parseInt(document.getElementById("select_6_4").value) + parseInt(document.getElementById("select_7_4").value) 
-    
-      var posz_wynik = parseInt(document.getElementById("select_1_5").value) + parseInt(document.getElementById("select_2_5").value) 
-      + parseInt(document.getElementById("select_3_5").value) + parseInt(document.getElementById("select_4_5").value) 
-      + parseInt(document.getElementById("select_5_5").value) + parseInt(document.getElementById("select_6_5").value) + parseInt(document.getElementById("select_7_5").value) 
-    
-      var an_wynik = parseInt(document.getElementById("select_1_6").value) + parseInt(document.getElementById("select_2_6").value) 
-      + parseInt(document.getElementById("select_3_6").value) + parseInt(document.getElementById("select_4_6").value) 
-      + parseInt(document.getElementById("select_5_6").value) + parseInt(document.getElementById("select_6_6").value) + parseInt(document.getElementById("select_7_6").value) 
-    
-      var gg_wynik = parseInt(document.getElementById("select_1_7").value) + parseInt(document.getElementById("select_2_7").value) 
-      + parseInt(document.getElementById("select_3_7").value) + parseInt(document.getElementById("select_4_7").value) 
-      + parseInt(document.getElementById("select_5_7").value) + parseInt(document.getElementById("select_6_7").value) + parseInt(document.getElementById("select_7_7").value) 
-    
-      var per_wynik = parseInt(document.getElementById("select_1_6").value) + parseInt(document.getElementById("select_2_1").value) 
-      + parseInt(document.getElementById("select_3_7").value) + parseInt(document.getElementById("select_4_4").value) 
-      + parseInt(document.getElementById("select_5_2").value) + parseInt(document.getElementById("select_6_6").value) + parseInt(document.getElementById("select_7_5").value) 
-    
-
-      if(!isNaN(document.getElementById("punkty_org").innerHTML))
-      {
-        document.getElementById("punkty_org").innerHTML
-        +=  
-        + org_wynik + "<br>";
-
-        document.getElementById("punkty_nl").innerHTML
-        +=  
-        + nl_wynik + "<br>";
-
-        document.getElementById("punkty_des").innerHTML
-        +=  
-        + des_wynik + "<br>";
-
-        document.getElementById("punkty_kre").innerHTML
-        +=  
-        + kre_wynik + "<br>";
-
-        document.getElementById("punkty_posz").innerHTML
-        +=  
-        + posz_wynik + "<br>";
-
-        document.getElementById("punkty_an").innerHTML
-        +=  
-        + an_wynik + "<br>";
-
-        document.getElementById("punkty_gg").innerHTML
-        +=  
-        + gg_wynik + "<br>";
-
-        document.getElementById("punkty_per").innerHTML
-        +=  
-        + per_wynik + "<br>";
-
-  
-      }
-
-    }
-  
-}
 
 function show_table()
 {
@@ -226,7 +48,7 @@ function show_table()
   
 }
 
-
+var is_pressed=false;
 function done(){
   var mentor=0;
   var A=0;
@@ -235,16 +57,14 @@ function done(){
   var D=0;
   var E=0;
   var F=0;
-
-  var coach=0;
-  var doradca=0;
+  
   var ele = document.getElementsByTagName('input');
     
 
        
 
 
-       // document.getElementById("disp").innerHTML = "";
+     
         var ele = document.getElementsByTagName('input');
         var d=[];
         for(i = 0; i < ele.length; i++) {
@@ -268,23 +88,27 @@ function done(){
   var p=Array.from(d);
  
 for(i=0;i<d.length;i++){
+
 if(d[i]=="A")A++; 
 if(d[i]=="B")B++; 
 if(d[i]=="C")C++; 
 if(d[i]=="D")D++;
 if(d[i]=="E")E++; 
 if(d[i]=="F")F++;
+}
 
-}  
-if(A+B+C+D+E+F==21){
-
+if(A+B+C+D+E+F==21 && (is_pressed==false)){
+  is_pressed=true;
+  console.log(is_pressed);
   if(A>C){
     document.getElementById("punkty_1").innerHTML
     +=  "W";
+   
   }
   if(A<C){
     document.getElementById("punkty_1").innerHTML
     +=  "K";
+    
   }
   if(D>F){
     document.getElementById("punkty_1").innerHTML
@@ -297,11 +121,15 @@ if(A+B+C+D+E+F==21){
   if(B>E){
     document.getElementById("punkty_1").innerHTML
     +=  "W";
+    
   }
   if(B<E){
     document.getElementById("punkty_1").innerHTML
     +=  "Z";
+   
   }
+
+}
 
 }
 
@@ -312,92 +140,10 @@ if(A+B+C+D+E+F==21){
 
 
 
-}
-
-
-function reset(id) {
-
-  for(var k=1;k<4;k++){//czyszczenie
-    for(var l=1;l<4;l++){
-
-      document.getElementById(id + "_"+k.toString() +"_"+ l.toString()  ).disabled = false;
-      document.getElementById(id + "_"+k.toString() +"_"+ l.toString()  ).checked = false;
-      //document.getElementById(id + "_"+k.toString() +"_"+ l.toString()  ).checked = false;
-
-      //document.getElementById("0_1_3").disabled = false;
-    }
-  }
-
-} 
 
 
 
 
 
-function display(id) { 
-
-   var checkRadio = document.querySelector('input[name="Question"]:checked');
-  var e = document.getElementById(id);
-var strUser = e.value;
-  //console.log(strUser); // en   
- // alert (strUser);
-  if(checkRadio != null) {
-      
-          
-         var question=id.slice(0, 1);
-          
-        
-          var number=id.slice(2, 3);
-          var point=id.slice(4, 5);
-          if(id.length==6)point=10;
-          var suma=0;
-
-        
 
 
-        for(var k=1;k<9;k++){ //sumowanie
-          for(var l=0;l<11;l++){
-           
-            if(document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).checked == true)
-            {
-             suma+= parseInt(document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).value)
-                //alert(suma);
-            }
-            
-          }
-        }
-        //czyszczenie 
-        for(var k=1;k<9;k++){ 
-          for(var l=0;l<11;l++){
-            
-            
-            if(parseInt(document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).value) + suma<=10)
-            {
-              if(!document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).checked == true)document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).disabled = false
-            
-            }
-            
-          }
-        }
-
-        console.log(suma);
-
-        //disabledowanie
-        for(var k=1;k<9;k++){ 
-          for(var l=0;l<11;l++){
-            
-            
-            if(parseInt(document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).value) + suma>10)
-            {
-              if(!document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).checked == true)document.getElementById(question + "_"+k.toString() +"_"+ l.toString()  ).disabled = true
-            
-            }
-            
-          }
-        }
-
- 
-  }
-  
-  
-}
